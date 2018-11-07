33e5a2f1e79019bf5c4ce007429d4d5163449d55
LeCleric <13441191+LeCleric@users.noreply.github.com>
2018-11-07 18:48:45 -0500

    Corrected a load of errors and added fancy log colours

--- src/main.py
+++ src/main.py
@@ -6,11 +6,15 @@ from processing import get_dataset
 while True:
     # Choose which dataset
     option1 = ""
-    while (int(option1) >= 0) and (int(option1) <= 2):
+    validOption = False
+    while not validOption:
         option1 = input(LogColors.HEADER + "Which dataset do you wish to use?\n" + LogColors.ENDC +
         "1. Dataset 1\n"
         "2. Dataset 2\n"
         + LogColors.FAIL + "0. Exit\n" + LogColors.ENDC)
+
+        if (int(option1) >= 0) and (int(option1) <= 2):
+            validOption = True
 
     if option1 == "0":
         exit(0)
@@ -37,35 +41,38 @@ while True:
     if option2 == "0":
         exit(0)
 
-    ds_training_features = None
-    ds_training_labels = None
-    ds_validation_features = None
-    ds_validation_labels = None
-
-    if option2 % 2 == 1:
+    if int(option2) % 2 == 1:
         ds_training_features, ds_training_labels = get_dataset("ds" + option1 + "/ds" + option1 + "Train.csv")
+
+        print(LogColors.OKGREEN + "Training..." + LogColors.ENDC)
+
+        if option2 == "1":
+            training.DT_train(ds_training_features, ds_training_labels)
+        elif option2 == "3":
+            training.MNB_train(ds_training_features, ds_training_labels)
+        elif option2 == "5":
+            training.BNB_train(ds_training_features, ds_training_labels)
+        elif option2 == "7":
+            training.GNB_train(ds_training_features, ds_training_labels)
+        elif option2 == "9":
+            training.CNB_train(ds_training_features, ds_training_labels)
+
+        print(LogColors.OKGREEN + "Trained Successfully ✔" + LogColors.ENDC)
     else:
-        ds_training_features, ds_training_labels = get_dataset("ds" + option1 + "/ds" + option1 + "Val.csv")
+        ds_validation_features, ds_validation_labels = get_dataset("ds" + option1 + "/ds" + option1 + "Val.csv")
 
-    if option2 == "1":
-        training.DT_train(ds_training_features, ds_training_labels)
-    elif option2 == "3":
-        training.MNB_train(ds_training_features, ds_training_labels)
-    elif option2 == "5":
-        training.BNB_train(ds_training_features, ds_training_labels)
-    elif option2 == "7":
-        training.GNB_train(ds_training_features, ds_training_labels)
-    elif option2 == "9":
-        training.CNB_train(ds_training_features, ds_training_labels)
+        print(LogColors.OKGREEN + "Validating..." + LogColors.ENDC)
 
-    elif option2 == "2":
-        validation.classify(ds_validation_features, ds_validation_labels, "DT")
-    elif option2 == "4":
-        validation.classify(ds_validation_features, ds_validation_labels, "MNB")
-    elif option2 == "6":
-        validation.classify(ds_validation_features, ds_validation_labels, "BNB")
-    elif option2 == "8":
-        validation.classify(ds_validation_features, ds_validation_labels, "GNB")
-    elif option2 == "10":
-        validation.classify(ds_validation_features, ds_validation_labels, "CNB")
+        if option2 == "2":
+            validation.classify(ds_validation_features, ds_validation_labels, "DT")
+        elif option2 == "4":
+            validation.classify(ds_validation_features, ds_validation_labels, "MNB")
+        elif option2 == "6":
+            validation.classify(ds_validation_features, ds_validation_labels, "BNB")
+        elif option2 == "8":
+            validation.classify(ds_validation_features, ds_validation_labels, "GNB")
+        elif option2 == "10":
+            validation.classify(ds_validation_features, ds_validation_labels, "CNB")
+
+        print(LogColors.OKGREEN + "Validated Successfully ✔" + LogColors.ENDC)
 
